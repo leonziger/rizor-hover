9216f33a4378b873a6d02359760b90312b19d333
Uma <buldog52@gmail.com>
2020-01-27 14:05:05 +0200

    Adaptive ready

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 window.$ = window.jQuery = require('jquery');
-
+import $ from 'jquery';
+require('waypoints/lib/jquery.waypoints');
 
 import './components/main-header/main-header';
 import './components/advantages/advantages';
 import './components/scroll-to-anchor/scroll-to-anchor';
 import './components/sticky-menu/sticky-menu';
+
+// const navli = document.querySelectorAll('.main-header__nav a');
+// console.log(navli);
+// const waypoint = new Waypoint({
+//     element: document.querySelectorAll('.ss'),
+//     handler: function(direction) {
+//         alert('You have scrolled to a thing')
+//     }
+// })
+
+// $('.track').waypoint({
+//     handler: function(direction) {
+//         // alert('ID:' + this.element.id);
+//         console.log(this.element.id);
+//     },
+//     offset: '20%'
+// });
--- src/components/scroll-to-anchor/scroll-to-anchor.js
+++ src/components/scroll-to-anchor/scroll-to-anchor.js
@@ -1,15 +1,66 @@
 import $ from 'jquery';
 
 
-const screenHeight = $(window).height();
-const screenWidth = $(window).width();
-  $('[href^="#"]').click(function(){
-    const mainHeader = document.querySelector('.main-header');
-    const link = $(this).attr('href');
-    $(this).addClass('active').siblings().removeClass('active');
-    let coordinates = $(link).offset().top;
+// const screenHeight = $(window).height();
+// const screenWidth = $(window).width();
+//   $('[href^="#"]').click(function(){
+//     const mainHeader = document.querySelector('.main-header');
+//     const link = $(this).attr('href');
+//     $(this).addClass('active').siblings().removeClass('active');
+//     let coordinates = $(link).offset().top;
+//
+//     $('html, body').animate({scrollTop:coordinates}, 1000);
+//     return false;
+//   });
 
-    $('html, body').animate({scrollTop:coordinates}, 1000);
-    return false;
-  });
 
+
+// Cache selectors
+let lastId,
+    topMenu = $(".main-header__nav"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+        let item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+console.log(menuItems);
+console.log(scrollItems);
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+    let href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 1000);
+    e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+    // Get container scroll position
+    let fromTop = $(this).scrollTop()+topMenuHeight;
+
+    // Get id of current scroll item
+    let cur = scrollItems.map(function(){
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length-1];
+    let id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        console.log(id);
+        console.log(menuItems.removeClass('active').end().filter("[href='#"+id+"']"));
+        // Set/remove active class
+        menuItems.removeClass('active').end().filter("[href='#"+id+"']").addClass('active');
+
+    }
+});
