64325aa7d4ea5313093efc08bff85fafdc5690a0
Uma <buldog52@gmail.com>
2020-01-28 00:02:12 +0200

    Modified scroll-to-anchor

--- src/components/scroll-to-anchor/scroll-to-anchor.js
+++ src/components/scroll-to-anchor/scroll-to-anchor.js
@@ -1,21 +1,6 @@
 import $ from 'jquery';
 
-
-// const screenHeight = $(window).height();
-// const screenWidth = $(window).width();
-//   $('[href^="#"]').click(function(){
-//     const mainHeader = document.querySelector('.main-header');
-//     const link = $(this).attr('href');
-//     $(this).addClass('active').siblings().removeClass('active');
-//     let coordinates = $(link).offset().top;
-//
-//     $('html, body').animate({scrollTop:coordinates}, 1000);
-//     return false;
-//   });
-
-
-
-// Cache selectors
+//Cache selectors
 let lastId,
     topMenu = $(".main-header__nav"),
     topMenuHeight = topMenu.outerHeight()+15,
@@ -27,14 +12,15 @@ let lastId,
         if (item.length) { return item; }
     });
 
-// console.log(menuItems);
-// console.log(scrollItems);
-
 // Bind click handler to menu items
 // so we can get a fancy scroll animation
+
 menuItems.click(function(e){
     let href = $(this).attr("href"),
         offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+
+    $('.active').removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
     $('html, body').stop().animate({
         scrollTop: offsetTop
     }, 1000);
@@ -44,23 +30,26 @@ menuItems.click(function(e){
 // Bind to scroll
 $(window).scroll(function(){
     // Get container scroll position
-    let fromTop = $(this).scrollTop()+topMenuHeight;
+    let windowOffset;
+    if ($(window).width() >= 1800 ) { windowOffset = 100;} else { windowOffset = 0; }
+    let fromTop = $(this).scrollTop() + topMenuHeight + windowOffset;
 
     // Get id of current scroll item
     let cur = scrollItems.map(function(){
         if ($(this).offset().top < fromTop)
             return this;
     });
+
     // Get the id of the current element
     cur = cur[cur.length-1];
     let id = cur && cur.length ? cur[0].id : "";
 
     if (lastId !== id) {
         lastId = id;
-        // console.log(id);
-        // console.log(menuItems.removeClass('active').end().filter("[href='#"+id+"']"));
         // Set/remove active class
-        menuItems.removeClass('active').end().filter("[href='#"+id+"']").addClass('active');
+        $('.active').removeClass('active');
+        menuItems.filter("[href='#"+id+"']").addClass('active').siblings().removeClass('active');
 
     }
 });
+
